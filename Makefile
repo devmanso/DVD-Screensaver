6a5d104e9b6286fac58d0d5504b7d4149b648995
devmanso <itsnothisuser@gmail.com>
2023-03-05 00:04:40 -0500

    initial commit

--- Makefile
+++ Makefile
@@ -1,2 +1,2 @@
 game:
-	gcc main.c -o play -I include -L lib -l SDL2-2.0.0
+	gcc src/main.c -o play -I include -L lib -l SDL2-2.0.0 -l SDL2_image -l lua
--- main.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <SDL2/SDL.h>
-
-int main() {
-
-    printf("INITIALISING.... \n");
-    SDL_Init(SDL_INIT_VIDEO);  
-    return 0;
-}
